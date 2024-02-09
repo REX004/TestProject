f86bab4116b53d8e17e8347c655f431deac94944
amirx <sergameramir2@gmail.com>
2024-02-09 12:12:11 +0300

    New commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,5 +79,6 @@ dependencies {
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
     implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.0")
 
+    implementation ("androidx.fragment:fragment:1.3.6")
 
 }
